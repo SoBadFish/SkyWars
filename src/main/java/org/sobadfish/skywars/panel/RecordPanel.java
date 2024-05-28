05ec6c7d31111ddf6a21d3e21838c9aa00916300
Sobadfish <3098143605>
2024-05-28 09:43:05 +0800

    修复秒人斧失效BUG

--- src/main/java/org/sobadfish/skywars/panel/RecordPanel.java
+++ src/main/java/org/sobadfish/skywars/panel/RecordPanel.java
@@ -1,12 +1,13 @@
 package org.sobadfish.skywars.panel;
 
-import cn.lanink.gamecore.form.windows.AdvancedFormWindowCustom;
-import cn.lanink.gamecore.form.windows.AdvancedFormWindowSimple;
 import cn.nukkit.Player;
+import cn.nukkit.form.element.ElementButton;
 import com.google.gson.JsonObject;
 import net.easecation.ghosty.LevelRecordPack;
 import org.sobadfish.skywars.manager.RecordManager;
 import org.sobadfish.skywars.manager.TotalManager;
+import org.sobadfish.skywars.panel.from.GameFrom;
+import org.sobadfish.skywars.panel.from.button.BaseIButton;
 
 import java.io.File;
 import java.util.List;
@@ -18,53 +19,92 @@ import java.util.List;
  */
 public class RecordPanel {
 
+    private static final String PLUGIN_NAME = "&l&cS&6k&ey&aW&ba&9r";
+
+
     public static void disPlayerMenu(Player player) {
-        AdvancedFormWindowSimple simple = new AdvancedFormWindowSimple(TotalManager.GAME_NAME + " - 录像管理", "请选择您要进行的操作");
-        simple.addButton("查看录像列表", RecordPanel::disRecordList);
-        simple.addButton("查找录像", RecordPanel::disRecordSearch);
-        simple.showToPlayer(player);
+
+        GameFrom simple = new GameFrom(PLUGIN_NAME + " - 录像管理","请选择您要进行的操作", GameFrom.getRId());
+        simple.add(new BaseIButton(new ElementButton("查看录像列表")) {
+            @Override
+            public void onClick(Player player) {
+                RecordPanel.disRecordList(player);
+            }
+        });
+        simple.add(new BaseIButton(new ElementButton("查找录像")) {
+            @Override
+            public void onClick(Player player) {
+                RecordPanel.disRecordSearch(player);
+            }
+        });
+        DisPlayWindowsFrom.FROM.put(player.getName(), simple);
+//        AdvancedFormWindowSimple simple = new AdvancedFormWindowSimple(PLUGIN_NAME + " - 录像管理", "请选择您要进行的操作");
+
+        simple.disPlay(player);
     }
 
     public static void disRecordList(Player player) {
-        AdvancedFormWindowSimple simple = new AdvancedFormWindowSimple(TotalManager.GAME_NAME + " - 录像列表", "请选择要操作的录像");
+        GameFrom simple = new GameFrom(PLUGIN_NAME + " - 录像列表","请选择要操作的录像", GameFrom.getRId());
+//        AdvancedFormWindowSimple simple = new AdvancedFormWindowSimple(PLUGIN_NAME + " - 录像列表", "请选择要操作的录像");
         List<File> list = TotalManager.getRecordManager().getRecordFileList();
         if (list != null && !list.isEmpty()) {
             for (File file : list) {
-                simple.addButton(file.getName().split("\\.")[0], cp -> disRecordInfo(player, file));
+                simple.add(new BaseIButton(new ElementButton(file.getName().split("\\.")[0])) {
+                    @Override
+                    public void onClick(Player player) {
+                        disRecordInfo(player, file);
+                    }
+                } );
             }
         } else {
-            simple.setContent("哎呀！还没有录像文件呢！快让玩家们玩一局吧！");
+            simple.setContext("哎呀！还没有录像文件呢！快让玩家们玩一局吧！");
         }
-        simple.addButton("返回", RecordPanel::disPlayerMenu);
-        simple.showToPlayer(player);
+        simple.add(new BaseIButton(new ElementButton("返回")) {
+            @Override
+            public void onClick(Player player) {
+                RecordPanel.disPlayerMenu(player);
+            }
+        });
+        DisPlayWindowsFrom.FROM.put(player.getName(), simple);
+        simple.disPlay(player);
     }
 
     public static void disRecordSearch(Player player) {
-        AdvancedFormWindowCustom custom = new AdvancedFormWindowCustom(TotalManager.GAME_NAME + " - 录像查找");
-
-        //TODO
-
-        custom.showToPlayer(player);
+//        AdvancedFormWindowCustom custom = new AdvancedFormWindowCustom(PLUGIN_NAME + " - 录像查找");
+//
+//        //TODO
+//
+//        custom.showToPlayer(player);
     }
 
     public static void disRecordInfo(Player player, File file) {
-        AdvancedFormWindowSimple simple = new AdvancedFormWindowSimple(TotalManager.GAME_NAME + " - 录像详细信息");
+        GameFrom simple = new GameFrom(PLUGIN_NAME + " - 录像详细信息","",GameFrom.getRId());
+//        AdvancedFormWindowSimple simple = new AdvancedFormWindowSimple(PLUGIN_NAME + " - 录像详细信息");
         LevelRecordPack recordPack = TotalManager.getRecordManager().getRecordPack(file);
         JsonObject metadata = recordPack.getMetadata();
-        simple.setContent("录像名称：" + file.getName() +
+        simple.setContext("录像名称：" + file.getName() +
                 "\n录像房间：" + metadata.getAsJsonPrimitive("roomName") +
                 "\n录像地图：" + metadata.getAsJsonPrimitive("roomWorld") +
                 "\n录制完成时间：" + metadata.getAsJsonPrimitive("time"));
-        simple.addButton("播放录像", cp -> {
-            RecordManager.OK ok = TotalManager.getRecordManager().playRecord(file, player);
-            if (ok.isOK()) {
-                player.sendMessage("开始播放录像：" + file.getName());
-            } else {
-                player.sendMessage("播放录像失败！原因：" + ok.getMessage());
+        simple.add(new BaseIButton(new ElementButton("播放录像")) {
+            @Override
+            public void onClick(Player player) {
+                RecordManager.OK ok = TotalManager.getRecordManager().playRecord(file, player);
+                if (ok.isOK()) {
+                    player.sendMessage("开始播放录像：" + file.getName());
+                } else {
+                    player.sendMessage("播放录像失败！原因：" + ok.getMessage());
+                }
             }
         });
-        simple.addButton("返回", cp -> disRecordList(player));
-        simple.showToPlayer(player);
+        simple.add(new BaseIButton(new ElementButton("返回")) {
+            @Override
+            public void onClick(Player player) {
+                disRecordList(player);
+            }
+        });
+        DisPlayWindowsFrom.FROM.put(player.getName(), simple);
+        simple.disPlay(player);
     }
 
 }
--- src/main/java/org/sobadfish/skywars/panel/from/GameFrom.java
+++ src/main/java/org/sobadfish/skywars/panel/from/GameFrom.java
@@ -6,6 +6,7 @@ import cn.nukkit.utils.TextFormat;
 import lombok.Getter;
 import lombok.Setter;
 import org.sobadfish.skywars.panel.from.button.BaseIButton;
+import org.sobadfish.skywars.tools.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,13 +20,17 @@ public class GameFrom {
 
     private final int id;
 
+    private final static int FROM_ID = 155;
+
+    private final static int FROM_MAX_ID = 105478;
+
     @Getter
     @Setter
     private List<BaseIButton> baseIButtons = new ArrayList<>();
 
     private final String title;
 
-    private final String context;
+    private String context;
     public GameFrom(String title, String context, int id){
         this.title = title;
         this.context = context;
@@ -49,8 +54,18 @@ public class GameFrom {
         player.showFormWindow(simple, getId());
     }
 
+    public static int getRId(){
+        return Utils.rand(FROM_ID,FROM_MAX_ID);
+    }
+    public static int getRId(int min,int max){
+        return Utils.rand(min,max);
+    }
     @Override
     public String toString() {
         return id+" -> "+ baseIButtons;
+    }
+
+    public void setContext(String s) {
+        this.context = s;
     }
 }
